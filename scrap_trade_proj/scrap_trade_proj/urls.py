895e82c74b99cf948caf5ac67c2f500bedd40622
Milan Kladivko <mikl@mikl-debian>
2020-01-07 09:45:23 +0100

    URL pattern name for language switch, used in `__header.html`.

--- scrap_trade_proj/scrap_trade_proj/urls.py
+++ scrap_trade_proj/scrap_trade_proj/urls.py
@@ -8,7 +8,7 @@ from django.conf.urls.i18n import i18n_patterns
 from django.utils.translation import gettext_lazy as _
 
 urlpatterns = [
-    path('i18n/', include('django.conf.urls.i18n')),
+    path('i18n/', include('django.conf.urls.i18n'), name='set_language'),
     path('', include('project_main.urls')),
     path('admin/', admin.site.urls),
     path('customers/', include('customers.urls')),
