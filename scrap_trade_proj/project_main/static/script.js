838f7b2d7fac58a12f9ca218bf0987f7b48adabc
Milan Kladivko <mikl@mikl-debian>
2020-03-03 11:29:19 +0100

    Calendar widget; Added working calendar widget for dates
    ...datetime extension coming up later.

--- scrap_trade_proj/project_main/static/script.js
+++ scrap_trade_proj/project_main/static/script.js
@@ -177,5 +177,293 @@ const login = (function() {
         'show_popup': show_popup,
         'close_popup': close_popup
     }
-    return public_api
-})()
+    return public_api;
+})();
+
+
+var calendar_widget = (function CalendarWidget() {
+
+    // Formatting to/from django
+    function django_encode(date) {
+        var month = '' + (date.getMonth() + 1);
+        var day = '' + date.getDate();
+        var year = '' + date.getFullYear();
+        // Pad with zeroes
+        if (month.length == 1) month = '0' + month;
+        if (day.length == 1) day = '0' + day;
+        return [year, month, day].join('-')
+    }
+    function django_decode(date_string) {
+        var comps = date_string.split('-');
+        // @todo; Make decoding dates in JS a bit more robust
+        return new Date(
+            comps[0],      // year
+            comps[1] - 1,  // month
+            comps[2]       // day
+        );
+    }
+
+    // Utility functions
+    function copy_of(node) {
+        const CLONE_CHILDREN = true;
+        var copy = node.cloneNode(CLONE_CHILDREN);
+        copy.id = '';  // Copy can't have the ID
+        return copy;
+    };
+    function capitalize(str) {
+        return str.charAt(0).toUpperCase() + str.substring(1);
+    };
+
+    
+
+    // Find the inputs viable for widgeting
+    const INPUT_SELECTOR = 'input[type="text"].dateinput.form-control';
+    const INPUTS = document.querySelectorAll(INPUT_SELECTOR);
+    if (INPUTS.length == 0) {
+        console.info("Calendar widget; No inputs for widget found.");
+        return;
+    }
+
+    // Grab HTML templates
+    const WrapperTemplate = document.getElementById('Calendar-Template');
+    const RowTemplate = document.getElementById('CalendarRow-Template');
+    const ItemTemplate = document.getElementById('CalendarItem-Template');
+    const MonthTemplate = document.getElementById('CalendarMonth-Template');
+    if (!WrapperTemplate || !RowTemplate ||
+        !ItemTemplate || !MonthTemplate) {
+        console.error("Calendar widget; " +
+                      "Templates not found, inputs won't use calendars.");
+        console.error(WrapperTemplate, RowTemplate,
+                      ItemTemplate, MonthTemplate);
+        return;
+    }
+
+    
+    INPUTS.forEach(function init_input(input) {
+        
+        const InitialDate = django_decode(input.value);
+        var CurrentlyDisplayed = null;  // Set when switching
+
+        setTimeout(function() {  // Defer to the end
+            force_select_date(InitialDate);
+        }, 1);
+        
+        // Element construction
+        var Wrapper = null;
+        (function construct_calendar_skeleton() {
+            
+            Wrapper = copy_of(WrapperTemplate);
+            input.parentNode.appendChild(Wrapper);
+            
+            // Weekdays header
+            const weekdays = Wrapper.querySelector('.calendar__weekdays');
+            DAYS.forEach(function(translated_day_name) {
+                const day = copy_of(ItemTemplate);
+                weekdays.appendChild(day);
+                var first_few = translated_day_name.substring(0, 2);
+                day.innerHTML = capitalize(first_few);
+            });
+
+            // Month switching events
+            const prev_btn = Wrapper.querySelector('button.Prev-Month');
+            prev_btn.addEventListener('click', function() {
+                var new_date = new Date(CurrentlyDisplayedMonth);  // Clone
+                new_date.setMonth(CurrentlyDisplayedMonth.getMonth() - 1);
+                switch_to_month(new_date);
+            });
+            const next_btn = Wrapper.querySelector('button.Next-Month');
+            next_btn.addEventListener('click', function() {
+                var new_date = new Date(CurrentlyDisplayedMonth);  // Clone
+                new_date.setMonth(CurrentlyDisplayedMonth.getMonth() + 1);
+                switch_to_month(new_date);
+            });
+        })();
+        
+        function switch_to_month(date_obj) {
+
+            // Internal value
+            CurrentlyDisplayedMonth = new Date(date_obj);  // Clone date
+            CurrentlyDisplayedMonth.setDate(1);  // Just in case...
+            
+            // Switch the calendar heading
+            Wrapper.querySelector('.Top-Date')
+                .innerHTML =
+                capitalize(MONTHS[date_obj.getMonth()]) + ' ' +
+                date_obj.getFullYear();
+
+            // Find the corresponding month
+            var wraps = Wrapper.querySelectorAll('.Month-Wrap');
+            var show_month = (function find_wrap_for_month(date) {
+                var found = null;
+                wraps.forEach(function(wrap) {
+                    if (wrap.dataset.month == date.getMonth()) {
+                        found = wrap;
+                        // break;  // Can't `break` out of a function!
+                    }
+                });
+                return found;
+            })(date_obj);
+
+            // Construct the wanted month if it doesn't exist
+            if (show_month === null) {
+                show_month = construct_month(date_obj); 
+            }
+
+            // Show only the month that we want to see  @todo; With effects
+            wraps.forEach(function(wrap) {
+                if (wrap == show_month) {
+                    wrap.classList.remove('d-none');
+                } else {
+                    wrap.classList.add('d-none');
+                }
+            });
+
+            return show_month;  // Return the month we've decided to show
+        };
+
+        function force_select_date(date) {
+            // Get the month with the date, (maybe create) and display it
+            var shown_month = switch_to_month(date);
+
+            // Find the button that corresponds with the date and click it
+            var dates = shown_month.querySelectorAll('.Date');
+            dates.forEach(function(date_elem) {
+                var data_attr = date_elem.dataset;
+                if (date.getFullYear() == data_attr.year &&
+                    date.getMonth() == data_attr.month &&
+                    date.getDate() == data_attr.day
+                   ) {
+                    date_elem.click();  // Trigger the click event
+                }
+            });
+        }
+        
+        function construct_month(date_obj) {
+            const month_wrap = Wrapper.querySelector('.calendar__months');
+            
+            const Month = copy_of(MonthTemplate);
+            month_wrap.appendChild(Month);
+            Month.dataset.month = date_obj.getMonth();
+            
+
+            // Padding out the months' starting day
+            
+            const month_starts_at_day = (function(date) {
+                const first_day = new Date(
+                    date.getFullYear(), date.getMonth(), 1);
+                var american_format = first_day.getDay();
+                if (american_format == 0) {
+                    return 6;  // Sunday fix
+                }
+                return american_format - 1;
+            })(date_obj);
+            
+            var row_i = 0;  // How filled is the current row
+            var current_row = copy_of(RowTemplate);
+            
+            current_row.innerHTML = '';
+            Month.appendChild(current_row);
+
+            
+            for (var day=0; day < month_starts_at_day; day++) {
+                current_row.appendChild(copy_of(ItemTemplate));
+                row_i += 1;
+            }
+
+            
+            // Actual days in the currently viewed month
+            
+            const days_in_this_month = (function(date) {
+                const last_day = new Date(
+                    date.getFullYear(),
+                    date.getMonth() + 1,  // Next month
+                    0  // 0th day wraps and gets last day of previous month
+                );
+                return last_day.getDate();
+            })(date_obj);
+            const today_day_index = (function(date) {
+                var today_date = new Date();
+                if (date.getFullYear() === today_date.getFullYear()) {
+                    if (date.getMonth() === today_date.getMonth()){
+                        return today_date.getDate();
+                    }
+                }
+                return -1;  // Today is not in current month
+            })(date_obj);
+            
+            for (var day=0; day < days_in_this_month; day++) {
+                
+                // Break line of days before writing Monday
+                var is_new_week = (row_i == 7);
+                if (is_new_week) {
+                    current_row = copy_of(RowTemplate);
+                    Month.appendChild(current_row);
+                    is_new_week = false;
+                    row_i = 0;
+                }
+                
+                const new_day = copy_of(ItemTemplate);
+                new_day.innerHTML = (day + 1) + '';
+                new_day.classList.add('Date', 'btn', 'big');
+                
+                // Save date into the HTML attributes
+                const data_attr = new_day.dataset;
+                data_attr.day = day + 1;
+                data_attr.month = date_obj.getMonth();
+                data_attr.year = date_obj.getFullYear();
+
+                // Click events for choosing the date
+                new_day.addEventListener('click', function() {
+
+                    const selected_date = (function(day_button) {
+                        const data_attr = day_button.dataset;
+                        return django_encode(new Date(
+                            data_attr.year, data_attr.month, data_attr.day
+                        ));
+                    })(this);
+                    
+                    const input = (function(calendar) {
+                        // Find nearest input element
+                        const widget_parent = calendar.parentNode;
+                        return widget_parent.querySelector('input');
+                    })(Wrapper);
+
+                    input.value = selected_date;
+
+                    // Toggle the selected item classes
+                    Wrapper.querySelectorAll('.SELECTED_DAY')
+                        .forEach(function(el) {
+                            el.classList.remove('SELECTED_DAY', 'btn-primary');
+                        });
+                    this.classList.add('SELECTED_DAY', 'btn-primary');
+                    
+                });
+                
+                // Highlight today, if viewing the correct month
+                if (today_day_index > 0) {
+                    const is_today = ((day + 1) === today_day_index);
+                    if (is_today) {
+                        new_day.style.boxShadow =
+                            '0px 0px 0px 3px hsla(205, 100%, 50%, 0.5)';
+                    }
+                }
+                
+                current_row.appendChild(new_day);
+                row_i += 1;
+            }
+
+            
+            // Padding out the end
+            
+            for (var day=row_i; day < 7; day++) {
+                current_row.appendChild(copy_of(ItemTemplate));
+            }
+
+
+            // Return the created month
+            return Month;
+        }
+    });
+    
+})();
+
